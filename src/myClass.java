71735099e3e92f6a20773396f6217b092a9fc03c
hebarajabi <hebarajabi90@gmail.com>
2023-02-05 19:35:47 +0400

    update 2

--- src/myClass.java
+++ src/myClass.java
@@ -5,8 +5,83 @@ public class myClass {
 		// TODO Auto-generated method stub
 
 		
-		String myname="Heba";
-		System.out.println(myname);
+
+		/*
+		 * String , double , int , boolean , date , a , main programming - (for , if ,
+		 * while , break , countinue ) methods ( string methods , Math) 
+		 * oop ,
+		 * inheretance , object
+		 */
+
+//		String myName = "abedalraheem";
+//
+//		String myStudent;
+//
+//		myStudent = "heba";
+
+//		System.out.println(myName.contains("z"));
+//		System.out.println(myName.contains("a"));
+
+		;
+
+//		System.out.println(myName.toUpperCase().charAt(1));
+
+//		System.out.println(myName.concat("faisal"));
+
+//		String zzz = myName+" "+"faisal"; 
+//		
+//		System.out.println(zzz);
+
+//		double myDouble = -88.9;
+//	System.out.println(Math.round(myDouble));	
+//	System.out.println(Math.ceil(myDouble));
+//	System.out.println(Math.floor(myDouble));
+
+//System.out.println(Math.absExact((int) myDouble));
+//; 
+//System.out.println(Math.abs(myDouble));
+//
+//		boolean iLoveMySelf = false;
+//
+//		Date myTime = new Date();
+
+//		System.out.println(myTime);
+
+//		myTime.getHours();
+		
+		// ----------------Part 2 ----------------------
+//		for () {}	
+//		while () {}
+//		if() {}
+		
+		
+//		for(int i = 0 ; i < 100 ; i ++) {
+//			if (i%2==1) {
+//				if(i ==9 ||i==19||i==29||i==39) {
+//					continue; 
+//					break;
+//				}
+//			
+//
+//				}
+//				System.out.println(i);
+//
+//			}
+		int i = 3 ; 
+	
+		while(i == 3) {
+			
+			System.out.println(i);
+			i++ ; 
+		}
+		
+		System.out.println(i);
+	
+
+	}
+		
+		
+		
 	}
 
-}
+
